80f0a950e4cd0873f09e78a37ad98111ed864bb4
Jan Ossowski <jan.ossowski@tink.se>
2020-04-23 14:42:07 +0200

    remove lombok

--- src/main/java/com/tink/recruitment/httpclient/healthcheck/HealthcheckFetcher.java
+++ src/main/java/com/tink/recruitment/httpclient/healthcheck/HealthcheckFetcher.java
@@ -1,6 +1,6 @@
 package com.tink.recruitment.httpclient.healthcheck;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -8,13 +8,17 @@ import org.springframework.web.client.RestTemplate;
 
 
 @Component
-@RequiredArgsConstructor
 public class HealthcheckFetcher {
 
     @Value("${base.url}")
     private String baseUrl;
 
     private final RestTemplate restTemplate;
+
+    @Autowired
+    public HealthcheckFetcher(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public HealthcheckResponse fetchData() {
         ResponseEntity<HealthcheckResponse> response = restTemplate.getForEntity(baseUrl + "/api/healthcheck", HealthcheckResponse.class);
